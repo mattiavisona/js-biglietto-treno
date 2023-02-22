f680691fdfcaba40417aeeb0737cc569699d9995
mattiavisona <mattiav2706@gmail.com>
2023-02-22 17:42:09 +0100

    prima parte esercizio

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// Nuovo File JS
+
+// INIZIO ESERCIZIO
+//_________________________________________________________________________________
+
+/*
+
+Prima parte esercizio
+- Chiedere all'utente il numero di kilometri che vuole percorrere
+- Chiedere all'utente l'età del passeggero
+- Memorizzarle sulla console
+
+*/
+
+let kilometriUser = prompt("Inserisci il numero di km che dovrai percorrere");
+console.log(kilometriUser);
+
+
+let etàUser = prompt("Inserisci la tua età");
+console.log(etàUser);
