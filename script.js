629c628ec9e8de04cec0354efe32bbd32f3666d8
mattiavisona <mattiav2706@gmail.com>
2023-02-22 17:54:11 +0100

    exercise completed

--- script.js
+++ script.js
@@ -14,9 +14,30 @@ Prima parte esercizio
 
 */
 
+
+/*
+
+Seconda parte dell'esercizio
+- Applicare lo sconto del 20% per i minorenni
+- Applicare lo sconto del 40% per gli over 65
+- Indicare il prezzo scontato e non in pagina
+
+*/
+
 let kilometriUser = prompt("Inserisci il numero di km che dovrai percorrere");
 console.log(kilometriUser);
 
 
 let etàUser = prompt("Inserisci la tua età");
 console.log(etàUser);
+
+let prezzoBiglietto = parseFloat(kilometriUser) * 0.21;
+
+if (etàUser < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.8;
+}
+else if (etàUser > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6;
+}
+
+document.writeln("Il prezzo del biglietto è: " + prezzoBiglietto.toFixed(2));
